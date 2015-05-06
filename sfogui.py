1f6b75d641e0a3c175c126f3744fe62af5c56636
Sencer H. <recnes@recnes.com>
2015-05-06 18:21:35 +0300

    Update sfogui.py
    some variable names refactored

--- sfogui.py
+++ sfogui.py
@@ -16,7 +16,7 @@ import wx.xrc
 
 class MainFrame(wx.Frame):
     def __init__(self, parent):
-        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"TTFO", pos=wx.DefaultPosition, size=wx.Size(800, 600),
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"Application", pos=wx.DefaultPosition, size=wx.Size(800, 600),
                           style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
 
         self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
@@ -25,11 +25,11 @@ class MainFrame(wx.Frame):
 
         bSizer7 = wx.BoxSizer(wx.HORIZONTAL)
 
-        sbSizer4 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"TT Giriş"), wx.VERTICAL)
+        sbSizer4 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"Login"), wx.VERTICAL)
 
         gSizer4 = wx.GridSizer(5, 2, 0, 0)
 
-        self.m_staticText8 = wx.StaticText(self, wx.ID_ANY, u"Kullanıcı", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText8 = wx.StaticText(self, wx.ID_ANY, u"User", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText8.Wrap(-1)
         gSizer4.Add(self.m_staticText8, 0, wx.ALL, 5)
 
@@ -37,7 +37,7 @@ class MainFrame(wx.Frame):
                                      wx.TE_READONLY)
         gSizer4.Add(self.user_name, 0, wx.ALL, 5)
 
-        self.m_staticText9 = wx.StaticText(self, wx.ID_ANY, u"Parola", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText9 = wx.StaticText(self, wx.ID_ANY, u"Pass", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText9.Wrap(-1)
         gSizer4.Add(self.m_staticText9, 0, wx.ALL, 5)
 
@@ -45,7 +45,7 @@ class MainFrame(wx.Frame):
                                      wx.TE_PASSWORD | wx.TE_READONLY)
         gSizer4.Add(self.user_pass, 0, wx.ALL, 5)
 
-        self.m_staticText10 = wx.StaticText(self, wx.ID_ANY, u"Şifre", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText10 = wx.StaticText(self, wx.ID_ANY, u"Key", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText10.Wrap(-1)
         gSizer4.Add(self.m_staticText10, 0, wx.ALL, 5)
 
@@ -53,24 +53,24 @@ class MainFrame(wx.Frame):
                                      wx.TE_PASSWORD | wx.TE_READONLY)
         gSizer4.Add(self.user_code, 0, wx.ALL, 5)
 
-        self.m_staticText11 = wx.StaticText(self, wx.ID_ANY, u"Resim", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText11 = wx.StaticText(self, wx.ID_ANY, u"Image", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText11.Wrap(-1)
         gSizer4.Add(self.m_staticText11, 0, wx.ALL, 5)
 
         self.captcha_text = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
         gSizer4.Add(self.captcha_text, 0, wx.ALL, 5)
 
-        self.resim_al = wx.Button(self, wx.ID_ANY, u"Resim Al", wx.DefaultPosition, wx.DefaultSize, 0)
-        gSizer4.Add(self.resim_al, 1, wx.ALL | wx.EXPAND, 5)
+        self.fetch_image = wx.Button(self, wx.ID_ANY, u"Fetch Image", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer4.Add(self.fetch_image, 1, wx.ALL | wx.EXPAND, 5)
 
-        self.giris_yap = wx.Button(self, wx.ID_ANY, u"Giriş Yap", wx.DefaultPosition, wx.DefaultSize, 0)
-        gSizer4.Add(self.giris_yap, 1, wx.ALL | wx.EXPAND, 5)
+        self.sign_in = wx.Button(self, wx.ID_ANY, u"Sign In", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer4.Add(self.sign_in, 1, wx.ALL | wx.EXPAND, 5)
 
         sbSizer4.Add(gSizer4, 1, wx.EXPAND, 5)
 
         bSizer7.Add(sbSizer4, 1, wx.EXPAND, 5)
 
-        sbSizer5 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"Uyarı Akışı"), wx.VERTICAL)
+        sbSizer5 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"Notices"), wx.VERTICAL)
 
         self.uyari_akisi = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
                                        wx.TE_MULTILINE | wx.TE_READONLY)
@@ -81,28 +81,28 @@ class MainFrame(wx.Frame):
 
         bSizer7.Add(sbSizer5, 1, wx.EXPAND, 5)
 
-        sbSizer6 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"İşlem Bilgileri"), wx.VERTICAL)
+        sbSizer6 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"Proc Info"), wx.VERTICAL)
 
         gSizer5 = wx.GridSizer(5, 2, 0, 0)
 
-        self.m_staticText12 = wx.StaticText(self, wx.ID_ANY, u"Ödenebilen Fatura", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText12 = wx.StaticText(self, wx.ID_ANY, u"Proc Count", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText12.Wrap(-1)
         gSizer5.Add(self.m_staticText12, 0, wx.ALL, 5)
 
-        self.fatura_adet = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+        self.proc_count = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
                                        wx.TE_READONLY | wx.ALIGN_RIGHT)
-        gSizer5.Add(self.fatura_adet, 0, wx.ALL, 5)
+        gSizer5.Add(self.proc_count, 0, wx.ALL, 5)
 
-        self.m_staticText13 = wx.StaticText(self, wx.ID_ANY, u"Toplam Ödeme Tutarı", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText13 = wx.StaticText(self, wx.ID_ANY, u"Total", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText13.Wrap(-1)
         gSizer5.Add(self.m_staticText13, 0, wx.ALL, 5)
 
-        self.fatura_tutar = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+        self.proctotal = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
                                         wx.TE_READONLY | wx.ALIGN_RIGHT)
-        gSizer5.Add(self.fatura_tutar, 0, wx.ALL, 5)
+        gSizer5.Add(self.proctotal, 0, wx.ALL, 5)
 
 
-        self.m_staticText15 = wx.StaticText(self, wx.ID_ANY, u"Ödeyici Bakiyesi", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText15 = wx.StaticText(self, wx.ID_ANY, u"Remote Balance", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText15.Wrap(-1)
         gSizer5.Add(self.m_staticText15, 0, wx.ALL, 5)
 
@@ -111,18 +111,18 @@ class MainFrame(wx.Frame):
         gSizer5.Add(self.api_balance_display, 0, wx.ALL, 5)
 
 
-        self.m_staticText14 = wx.StaticText(self, wx.ID_ANY, u"TTS Bakiyesi", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText14 = wx.StaticText(self, wx.ID_ANY, u"Local Balance", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_staticText14.Wrap(-1)
         gSizer5.Add(self.m_staticText14, 0, wx.ALL, 5)
 
-        self.kalan_bakiye = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+        self.last_balance = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
                                         wx.TE_READONLY | wx.ALIGN_RIGHT)
-        gSizer5.Add(self.kalan_bakiye, 0, wx.ALL, 5)
+        gSizer5.Add(self.last_balance, 0, wx.ALL, 5)
 
-        self.odeme_basla = wx.Button(self, wx.ID_ANY, u"Ödemeye Başla", wx.DefaultPosition, wx.DefaultSize, 0)
-        gSizer5.Add(self.odeme_basla, 1, wx.ALL | wx.EXPAND, 5)
+        self.start_proc = wx.Button(self, wx.ID_ANY, u"Start", wx.DefaultPosition, wx.DefaultSize, 0)
+        gSizer5.Add(self.start_proc, 1, wx.ALL | wx.EXPAND, 5)
 
-        self.odeme_dur = wx.Button(self, wx.ID_ANY, u"Ödemeyi Durdur", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.odeme_dur = wx.Button(self, wx.ID_ANY, u"Stop", wx.DefaultPosition, wx.DefaultSize, 0)
         gSizer5.Add(self.odeme_dur, 1, wx.ALL | wx.EXPAND, 5)
 
         sbSizer6.Add(gSizer5, 1, wx.EXPAND, 5)
@@ -133,9 +133,9 @@ class MainFrame(wx.Frame):
 
         bSizer8 = wx.BoxSizer(wx.VERTICAL)
 
-        self.FaturaListesi = wx.ListCtrl(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+        self.ProcList = wx.ListCtrl(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
                                          wx.LC_NO_SORT_HEADER | wx.LC_REPORT)
-        bSizer8.Add(self.FaturaListesi, 1, wx.ALL | wx.EXPAND, 5)
+        bSizer8.Add(self.ProcList, 1, wx.ALL | wx.EXPAND, 5)
 
         self.stat_gauge = wx.Gauge(self, wx.ID_ANY, 100, wx.DefaultPosition, wx.DefaultSize, wx.GA_HORIZONTAL)
         self.stat_gauge.SetValue(0)
@@ -149,10 +149,10 @@ class MainFrame(wx.Frame):
         self.Centre(wx.BOTH)
 
         # Connect Events
-        self.resim_al.Bind(wx.EVT_BUTTON, self.fetch_captcha)
-        self.giris_yap.Bind(wx.EVT_BUTTON, self.do_login)
-        self.odeme_basla.Bind(wx.EVT_BUTTON, self.odeme_baslat)
-        self.odeme_dur.Bind(wx.EVT_BUTTON, self.odeme_durdur)
+        self.fetch_image.Bind(wx.EVT_BUTTON, self.fetch_captcha)
+        self.sign_in.Bind(wx.EVT_BUTTON, self.do_login)
+        self.start.Bind(wx.EVT_BUTTON, self.starter)
+        self.stop.Bind(wx.EVT_BUTTON, self.stoper)
 
     def __del__(self):
         pass
@@ -165,8 +165,8 @@ class MainFrame(wx.Frame):
     def do_login(self, event):
         event.Skip()
 
-    def odeme_baslat(self, event):
+    def starter(self, event):
         event.Skip()
 
-    def odeme_durdur(self, event):
+    def stoper(self, event):
         event.Skip()
